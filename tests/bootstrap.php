6da9233b2b517f3171ee2e6836861b7155c68666
Andrew Welch <andrew@nystudio107.com>
2024-07-02 17:39:48 -0400

    refactor: Fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
-define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__, 4) . '/vendor');
+echo dirname(__DIR__) . '/vendor';
+define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__) . '/vendor');
 
 require realpath(PROJECT_VENDOR_DIR . '/autoload.php');
