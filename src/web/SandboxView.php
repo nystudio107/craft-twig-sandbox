c34b25ee1b9fef1f626728a3a2e61fc88b60f255
Andrew Welch <andrew@nystudio107.com>
2025-02-17 19:02:51 -0500

    feat: Craft Twig Sandbox no longer automatically handles exceptions when rendering sandbox templates. Instead, you can decide whether to handle the exception yourself, or pass it along to the `sandboxErrorHandler` for display in the browser/console

--- src/web/SandboxView.php
+++ src/web/SandboxView.php
@@ -7,7 +7,6 @@ use craft\web\View;
 use nystudio107\crafttwigsandbox\console\SandboxErrorHandler as ConsoleSandboxErrorHandler;
 use nystudio107\crafttwigsandbox\twig\BlacklistSecurityPolicy;
 use nystudio107\crafttwigsandbox\web\SandboxErrorHandler as WebSandboxErrorHandler;
-use Throwable;
 use Twig\Extension\SandboxExtension;
 use Twig\Sandbox\SecurityPolicyInterface;
 
@@ -21,13 +20,10 @@ class SandboxView extends View
      */
     public ?SecurityPolicyInterface $securityPolicy = null;
 
-    // Protected Properties
-    // =========================================================================
-
     /**
      * @var WebSandboxErrorHandler|ConsoleSandboxErrorHandler|null The error handler to use for the SandboxView
      */
-    protected WebSandboxErrorHandler|ConsoleSandboxErrorHandler|null $sandboxErrorHandler = null;
+    public WebSandboxErrorHandler|ConsoleSandboxErrorHandler|null $sandboxErrorHandler = null;
 
     // Public Methods
     // =========================================================================
@@ -43,50 +39,5 @@ class SandboxView extends View
         $this->securityPolicy = $this->securityPolicy ?? new BlacklistSecurityPolicy();
         // Add the SandboxExtension with our SecurityPolicy lazily via ::registerTwigExtension()
         $this->registerTwigExtension(new SandboxExtension($this->securityPolicy, true));
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function renderObjectTemplate(string $template, mixed $object, array $variables = [], string $templateMode = self::TEMPLATE_MODE_SITE): string
-    {
-        $result = '';
-        try {
-            $result = parent::renderObjectTemplate($template, $object, $variables, $templateMode);
-        } catch (Throwable $e) {
-            $this->sandboxErrorHandler->handleException($e);
-        }
-
-        return $result;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function renderString(string $template, array $variables = [], string $templateMode = self::TEMPLATE_MODE_SITE, bool $escapeHtml = false): string
-    {
-        $result = '';
-        try {
-            $result = parent::renderString($template, $variables, $templateMode, $escapeHtml);
-        } catch (Throwable $e) {
-            $this->sandboxErrorHandler->handleException($e);
-        }
-
-        return $result;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function renderTemplate(string $template, array $variables = [], ?string $templateMode = null): string
-    {
-        $result = '';
-        try {
-            $result = parent::renderTemplate($template, $variables, $templateMode);
-        } catch (Throwable $e) {
-            $this->sandboxErrorHandler->handleException($e);
-        }
-
-        return $result;
     }
 }
